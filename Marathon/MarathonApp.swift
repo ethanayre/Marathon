8773b9b0c54b8835bbad33a3461cabe875aa1d6a
ethan <ayre.ethan@yahoo.com>
2024-04-08 22:54:10 +0100

    Initial Commit

---
+++ Marathon/MarathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarathonApp.swift
+//  Marathon
+//
+//  Created by Ethan Ayre on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
