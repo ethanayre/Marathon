08ea39987b1c531645834b1ade34b1a33eee0322
ethan <ayre.ethan@yahoo.com>
2024-04-10 07:55:29 +0100

    Created the basic layout and addeed some key navigation features

--- Marathon/ContentView.swift
+++ Marathon/ContentView.swift
@@ -7,20 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State var name = "Ethan"
+    @State var daysLeft = 24
+
     var body: some View {
-        VStack {
-            Image(systemName: "1.circle")
-                .resizable()
-                .scaledToFit()
-            Text("Version 1.0")
-                .font(.largeTitle)
-                .fontWeight(.black)
+        
+
+        Spacer()
+        TabView {
+                    // First Tab
+                    VStack {
+                        HStack {
+                            Text("Marathon countdown")
+                            Image(systemName: "map.fill")
+                        }.font(.title).foregroundColor(.green)
+                        
+                Spacer(minLength: 40)
+                        
+                        VStack{
+                                    Text("Nearly there \(name)! You have \(daysLeft) days unitl your big run!")
+                        }
+                        Spacer()
+                        
+                        NavigationStack{
+                           
+                            List{
+                                NavigationLink ("Running plan") {
+                                    Text("Run plan").navigationTitle("Destination")}
+                            }
+                            }
+                    }
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                    
+                    // Second Tab
+                    VStack {
+                        Text("Content for Second Tab")
+                    }
+                    .tabItem {
+                        Image(systemName: "map")
+                        Text("Plan")
+                    }
+                    
+                    // Third Tab
+                    VStack {
+                        Text("Content for Third Tab")
+                    }
+                    .tabItem {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+                }
+
         }
-        .padding()
-        .foregroundColor(.red)
     }
-}
+
 
 #Preview {
     ContentView()
